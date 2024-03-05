eb8ef67ea95b805ca1c34e3cdad10ebfb78ce143
Ilya Dragonromeo <you@example.com>
2024-03-05 16:37:51 +0300

    feat: add Register component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,16 +1,21 @@
 import style from './Register.module.scss';
+import image from '../../assets/icons/image_icon.png';
 
 export const Register = () => {
   return (
     <div className={style.form_container}>
       <div className={style.form_wrapper}>
         <span className={style.logo}>Relex messenger</span>
-        <span className='title'>Register</span>
-        <form action=''>
+        <span className={style.title}>Register</span>
+        <form className={style.form} action=''>
           <input type='text' placeholder='display name' />
           <input type='email' placeholder='email' />
           <input type='password' placeholder='password' />
-          <input type='file' />
+          <input style={{ display: 'none' }} type='file' id='file' />
+          <label htmlFor='file'>
+            <img src={image} width='25px' alt='upload image icon' />
+            <span>Add an avatar</span>
+          </label>
           <button>Sign up</button>
         </form>
         <p>Do you have an account? Login </p>
