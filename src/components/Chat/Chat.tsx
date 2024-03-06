40b0389143387b97bbe8f5fe4f697853a5bcab38
Ilya Dragonromeo <you@example.com>
2024-03-06 15:26:51 +0300

    feat: add Chat component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,12 @@
 import style from './Chat.module.scss';
 
 export const Chat = () => {
-  return <div className={style.chat}>Chat</div>;
+  return (
+    <div className={style.chat}>
+      <div className={style.chat_info}>
+        <span>Jane</span>
+        <div className={style.chat_icons}></div>
+      </div>
+    </div>
+  );
 };
