a9267297490dc4c48ac2123ad3f3baa9d04c2cf8
Ilya Dragonromeo <you@example.com>
2024-03-06 17:38:11 +0300

    feat: add Message component

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,6 +1,18 @@
+import style from './Message.module.scss';
 
 export const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className={`${style.message} ${style.owner}`}>
+      <div className={style.message_info}>
+        <img
+          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
+        />
+        <span>just now</span>
+      </div>
+      <div className={style.message_content}>
+        <p>hello</p>
+      </div>
+    </div>
+  );
+};
