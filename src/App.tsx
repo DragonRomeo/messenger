114175a6be55195d231943061a023cf29eec9e8e
Ilya Dragonromeo <you@example.com>
2024-03-08 18:32:25 +0300

    feat: add currentUser state to check auth user

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
+import { FC, useEffect, useState } from 'react';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+
+interface Props {
+  children: JSX.Element;
+}
 
 function App() {
+  //maybe will need to add "{}" in useState for init state
+  const [currentUser, setCurrentUser] = useState<User | null>();
+
+  const ProtectedRoute: FC<Props> = ({ children }) => {
+    console.log('protected route');
+    if (!currentUser) {
+      console.log('protected route condition');
+      return <Navigate to='/login'></Navigate>;
+    }
+    return children;
+  };
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path='/'>
-          <Route index element={<Home />}></Route>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          ></Route>
           <Route path='login' element={<Login />}></Route>
           <Route path='register' element={<Register />}></Route>
         </Route>
--- src/components/Sidebar/components/Navbar/Navbar.tsx
+++ src/components/Sidebar/components/Navbar/Navbar.tsx
@@ -1,13 +1,18 @@
+import { signOut } from 'firebase/auth';
 import style from './Navbar.module.scss';
+import { auth } from '../../../../firebase';
 
 export const Navbar = () => {
   return (
     <div className={style.navbar}>
       <span className={style.logo}>Relex Chat</span>
       <div className={style.user}>
-        <img src='https://images.unsplash.com/photo-1708890628348-b36b599ae3b3?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+        <img
+          src='https://images.unsplash.com/photo-1708890628348-b36b599ae3b3?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          alt=''
+        />
         <span>John</span>
-        <button>logout</button>
+        <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
   );
