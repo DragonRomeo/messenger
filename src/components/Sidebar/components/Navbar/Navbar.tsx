8384fc7a1c30429b13532626e0f032310c851539
Ilya Dragonromeo <you@example.com>
2024-03-10 16:17:57 +0300

    feat: update Navbar logic

--- src/components/Sidebar/components/Navbar/Navbar.tsx
+++ src/components/Sidebar/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
 import { signOut } from 'firebase/auth';
 import style from './Navbar.module.scss';
 import { auth } from '../../../../firebase';
+import { useDataContext } from '../../../../context/context';
 
 export const Navbar = () => {
+  const { currentUser } = useDataContext();
+
   return (
     <div className={style.navbar}>
       <span className={style.logo}>Relex Chat</span>
       <div className={style.user}>
-        <img
-          src='https://images.unsplash.com/photo-1708890628348-b36b599ae3b3?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt=''
-        />
-        <span>John</span>
+        {currentUser?.photoURL && (
+          <img src={currentUser?.photoURL} alt='avatar' />
+        )}
+        <span>{currentUser?.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
     </div>
