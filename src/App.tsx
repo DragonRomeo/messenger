e800bbd49c1ff4f8fcd610f0a0176070f13d97c6
Ilya Dragonromeo <you@example.com>
2024-03-11 19:48:21 +0300

    fix: temporary fix curpage aft reload if user auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { FC, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import { DataContext } from './context/context';
 import { useDispatch } from 'react-redux';
 import { addAuthData } from './store/authSlice';
 
-interface Props {
-  children: JSX.Element;
-}
-
 function App() {
   //maybe will need to add "{}" in useState for init state
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const dispatch = useDispatch();
-
-  const ProtectedRoute: FC<Props> = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to='/login'></Navigate>;
-    }
-    return children;
-  };
+  //TODO: FIX LOGIN / AUTH RENDER LATER!!!
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
@@ -41,21 +31,14 @@ function App() {
     return () => {
       unsub();
     };
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
       <DataContext.Provider value={{ currentUser }}>
         <Routes>
           <Route path='/'>
-            <Route
-              index
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            ></Route>
+            <Route index element={currentUser ? <Home /> : <Login />}></Route>
             <Route path='login' element={<Login />}></Route>
             <Route path='register' element={<Register />}></Route>
           </Route>
