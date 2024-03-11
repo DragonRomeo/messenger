7c158e3a6dab0ffeb729cdca5eb314a56fcfe997
Ilya Dragonromeo <you@example.com>
2024-03-11 18:50:50 +0300

    feat: update Chats with main logic

--- src/components/Sidebar/components/Chats/Chats.tsx
+++ src/components/Sidebar/components/Chats/Chats.tsx
@@ -1,19 +1,44 @@
 import style from './Chats.module.scss';
 import style2 from '../Search/Search.module.scss';
+import { useEffect, useState } from 'react';
+import { DocumentData, doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../../../firebase';
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../../../store';
 
 export const Chats = () => {
+  const [chats, setChats] = useState<DocumentData | undefined>();
+
+  const currentUser = useSelector((state: IRootState) => state.auth.authData);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+        setChats(doc.data());
+
+        return () => {
+          unsub();
+        };
+      });
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  // console.log(chats);
+  console.log(chats && Object.entries(chats));
   return (
     <div className={style.chats}>
-      <div className={style2.user_chat}>
-        <img
-          src='https://images.unsplash.com/photo-1708448152962-08e08e297e41?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          alt=''
-        />
-        <div className={style2.user_chat_info}>
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {chats &&
+        Object.entries(chats)?.map((chat) => (
+          <div className={style2.user_chat} key={chat[0]}>
+            <img src={chat[1].userInfo.photoURL} alt='avatar' />
+            <div className={style2.user_chat_info}>
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].userInfo.lastMessage?.text}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
