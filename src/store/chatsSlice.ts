1bebfd88e7e78f19fdf5cf505ff3e2158451bb19
Ilya Dragonromeo <you@example.com>
2024-03-12 22:04:21 +0300

    refactor: remove extra code from chatSlice

--- src/store/chatsSlice.ts
+++ src/store/chatsSlice.ts
@@ -1,25 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 interface IUser {
-  chatId: string;
   user: { displayName?: string; photoURL?: string; uid?: string };
 }
 
 const chatSlice = createSlice({
   name: 'chat',
   initialState: {
-    chatId: 'null',
     user: {},
   } as Partial<IUser>,
   reducers: {
     addChatData(state, action) {
       state.user = action.payload;
-      state.chatId;
-      /*  ! Selector's Logic
-      state.auth.authData.uid > action.payload.uid
-      ? state.auth.authData.uid + action.payload.uid
-      : action.payload.uid + state.auth.authData).uid;
-           */
     },
   },
 });
