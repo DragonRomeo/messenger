9171e4da7c836af13174ae043cfee086da0916a5
Ilya Dragonromeo <you@example.com>
2024-03-12 21:55:35 +0300

    refactor: move Message folder into MessageS

--- src/components/Chat/components/Messages/Messages.tsx
+++ src/components/Chat/components/Messages/Messages.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Message } from '../../../Message/Message';
+import { Message } from './Message/Message';
 import style from './Messages.module.scss';
 import { IRootState } from '../../../../store';
 import { useEffect, useState } from 'react';
