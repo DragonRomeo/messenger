fb47e6314c1ed4ac7a2022fc60f8a8617480dfde
Ilya Dragonromeo <you@example.com>
2024-03-13 20:18:26 +0300

    feat: add sorting in chats for date time

--- src/components/Sidebar/components/Chats/Chats.tsx
+++ src/components/Sidebar/components/Chats/Chats.tsx
@@ -30,21 +30,23 @@ export const Chats = () => {
   return (
     <div className={style.chats}>
       {chats &&
-        Object.entries(chats)?.map((chat) => (
-          <div
-            className={style2.user_chat}
-            key={chat[0]}
-            onClick={() => {
-              return dispatch(addChatData(chat[1].userInfo));
-            }}
-          >
-            <img src={chat[1]?.userInfo?.photoURL} alt='avatar' />
-            <div className={style2.user_chat_info}>
-              <span>{chat[1].userInfo?.displayName}</span>
-              <p>{chat[1].lastMessage?.text}</p>
+        Object.entries(chats)
+          ?.sort((a, b) => b[1].date - a[1].date)
+          .map((chat) => (
+            <div
+              className={style2.user_chat}
+              key={chat[0]}
+              onClick={() => {
+                return dispatch(addChatData(chat[1].userInfo));
+              }}
+            >
+              <img src={chat[1]?.userInfo?.photoURL} alt='avatar' />
+              <div className={style2.user_chat_info}>
+                <span>{chat[1].userInfo?.displayName}</span>
+                <p>{chat[1].lastMessage?.text}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
     </div>
   );
 };
