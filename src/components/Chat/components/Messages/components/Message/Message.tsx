7453a64b69aa5fff607ff7074e1bfa00bad7836e
Ilya Dragonromeo <you@example.com>
2024-03-13 21:27:53 +0300

    feat: add smooth to last msg

--- src/components/Chat/components/Messages/components/Message/Message.tsx
+++ src/components/Chat/components/Messages/components/Message/Message.tsx
@@ -1,17 +1,41 @@
+import { useSelector } from 'react-redux';
 import style from './Message.module.scss';
+import { IRootState } from '../../../../../../store';
+import { useEffect, useRef } from 'react';
 
 export const Message = ({ message }) => {
+  //TODO: I need separate file for selectors.
+  const currentUser = useSelector((state: IRootState) => state.auth.authData);
+  const secondUser = useSelector((state: IRootState) => state.chat.user);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [message]);
+
   return (
-    <div className={`${style.message} ${style.owner}`}>
+    <div
+      ref={ref}
+      className={
+        message.senderId === currentUser.uid
+          ? `${style.message} ${style.owner}`
+          : `${style.message}`
+      }
+    >
       <div className={style.message_info}>
         <img
-          src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : secondUser?.photoURL
+          }
           alt=''
         />
         <span>just now</span>
       </div>
       <div className={style.message_content}>
-        <p>hello</p>
+        <p>{message.text}</p>
       </div>
     </div>
   );
