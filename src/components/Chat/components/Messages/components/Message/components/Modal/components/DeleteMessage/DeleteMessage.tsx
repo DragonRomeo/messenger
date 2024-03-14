09a10e664a272afccf9a8823c9f72301bad9bab4
Ilya Dragonromeo <you@example.com>
2024-03-14 20:20:10 +0300

    feat: add delete message func in <DeleteMessage>

--- src/components/Chat/components/Messages/components/Message/components/Modal/components/DeleteMessage/DeleteMessage.tsx
+++ src/components/Chat/components/Messages/components/Message/components/Modal/components/DeleteMessage/DeleteMessage.tsx
@@ -1,11 +1,40 @@
-import { DocumentData } from 'firebase/firestore';
+import { DocumentData, arrayRemove, doc, updateDoc } from 'firebase/firestore';
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { selectors } from '../../../../../../../../../../models/selectors/selectors';
+import { db } from '../../../../../../../../../../firebase';
 
 interface Props {
   style: string;
   message: DocumentData;
 }
 
-export const DeleteMessage: FC<Props> = ({ style }) => {
-  return <div className={style}>Delete Message</div>;
+export const DeleteMessage: FC<Props> = ({ style, message }) => {
+  const chatID = useSelector(selectors.chatID);
+
+  const handleDelete = async () => {
+    if (!chatID) {
+      return;
+    }
+
+    new Promise((resolve) => {
+      //This will delete this object on firestore
+      resolve(
+        updateDoc(doc(db, 'chats', chatID), {
+          messages: arrayRemove({
+            id: message.id,
+            text: message.text,
+            senderId: message.senderId,
+            date: message.date,
+          }),
+        })
+      );
+    });
+  };
+
+  return (
+    <div className={style} onClick={handleDelete}>
+      Delete Message
+    </div>
+  );
 };
