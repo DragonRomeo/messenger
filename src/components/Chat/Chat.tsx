a39623d58b16b2592a31429ab78e383e57934b4a
Ilya Dragonromeo <you@example.com>
2024-03-17 18:25:36 +0300

    feat: add filter button

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,16 +3,39 @@ import { Messages } from './components/Messages/Messages';
 import style from './Chat.module.scss';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../store';
+import { useState } from 'react';
+import icon from '../../assets/icons/icon_filter.png';
 
 export const Chat = () => {
+  const [isShowFilter, setIsShowFilter] = useState(false);
   //Write a selector for sumID
   const user = useSelector((state: IRootState) => state.chat.user);
+
+  const handleClick = () => {
+    setIsShowFilter((prevState) => !prevState);
+  };
 
   return (
     <div className={style.chat}>
       <div className={style.chat_info}>
         <span>{user?.displayName}</span>
-        <div className={style.chat_icons}></div>
+        {isShowFilter ? (
+          <div className={style.chat_icons}>
+            <div className={style.close_filter} onClick={handleClick}></div>
+            <div className={style.date_container}>
+              <span>Start</span>
+              <input type='date' />
+            </div>
+            <div className={style.date_container}>
+              <span>End</span>
+              <input type='date' />
+            </div>
+          </div>
+        ) : (
+          <button className={style.filter} onClick={handleClick}>
+            <img src={icon} alt='filter' />
+          </button>
+        )}
       </div>
       <Messages />
       <Input />
