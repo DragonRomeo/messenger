0864ddb06e07e1e3743b080f64aafb3cf0d5737d
Ilya Dragonromeo <you@example.com>
2024-03-18 16:51:56 +0300

    fix: fix login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
 import { DataContext } from './context/context';
 import { useDispatch } from 'react-redux';
 import { addAuthData } from './store/authSlice';
+
+interface Children {
+  children: React.ReactElement;
+}
 
 function App() {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
@@ -31,12 +35,26 @@ function App() {
     };
   }, [dispatch]);
 
+  const ProtectedRoute = ({ children }: Children) => {
+    if (!currentUser) {
+      return <Navigate to='/login' />;
+    }
+    return children;
+  };
+
   return (
     <>
       <DataContext.Provider value={{ currentUser }}>
         <Routes>
           <Route path='/'>
-            <Route index element={currentUser ? <Home /> : <Login />}></Route>
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            ></Route>
             <Route path='login' element={<Login />}></Route>
             <Route path='register' element={<Register />}></Route>
           </Route>
