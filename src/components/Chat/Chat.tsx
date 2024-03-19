f1d9b7410584a108a85ea6493cd20c914ae0c5f4
Ilya Dragonromeo <you@example.com>
2024-03-19 21:02:03 +0300

    feat: add empty user_filter icon & update styles

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -4,10 +4,12 @@ import style from './Chat.module.scss';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../store';
 import { useState } from 'react';
-import icon from '../../assets/icons/icon_filter.png';
+import date_filter_icon from '../../assets/icons/icon_filter.png';
+import user_filter_icon from '../../assets/icons/user_filter_icon.png';
 
 export const Chat = () => {
   const [isShowFilter, setIsShowFilter] = useState(false);
+  const [isShowUserFilter, setIsShowUserFilter] = useState(false);
   const [startDate, setStartData] = useState<string | null>(null);
   const [endDate, setEndData] = useState<string | null>(null);
   //Write a selector for sumID
@@ -17,31 +19,51 @@ export const Chat = () => {
     setIsShowFilter((prevState) => !prevState);
   };
 
+  const handleClickUser = () => {
+    setIsShowUserFilter((prevState) => !prevState);
+  };
+
   return (
     <div className={style.chat}>
       <div className={style.chat_info}>
         <span>{user?.displayName}</span>
-        {isShowFilter ? (
-          <div className={style.chat_icons}>
-            <div className={style.close_filter} onClick={handleClick}></div>
-            <div className={style.date_container}>
-              <span>Start</span>
-              <input
-                type='date'
-                onChange={(e) => setStartData(e.target.value)}
-              />
-            </div>
-            <div className={style.date_container}>
-              <span>End</span>
-              <input type='date' onChange={(e) => setEndData(e.target.value)} />
-            </div>
+        {user?.displayName && (
+          <div className={style.filters_container}>
+            {isShowFilter ? (
+              <div className={style.chat_icons}>
+                <div className={style.close_filter} onClick={handleClick}></div>
+                <div className={style.date_container}>
+                  <span>Start</span>
+                  <input
+                    type='date'
+                    onChange={(e) => setStartData(e.target.value)}
+                  />
+                </div>
+                <div className={style.date_container}>
+                  <span>End</span>
+                  <input
+                    type='date'
+                    onChange={(e) => setEndData(e.target.value)}
+                  />
+                </div>
+              </div>
+            ) : (
+              <button className={style.filter} onClick={handleClick}>
+                <img src={date_filter_icon} alt='date_filter' />
+              </button>
+            )}
+
+            {isShowUserFilter ? (
+              <></>
+            ) : (
+              <button className={style.filter} onClick={handleClickUser}>
+                <img src={user_filter_icon} alt='user_filter' />
+              </button>
+            )}
           </div>
-        ) : (
-          <button className={style.filter} onClick={handleClick}>
-            <img src={icon} alt='filter' />
-          </button>
         )}
       </div>
+
       <Messages startDate={startDate} endDate={endDate} />
       <Input />
     </div>
