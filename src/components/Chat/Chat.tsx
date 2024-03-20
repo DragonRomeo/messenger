6fc52aa5dc45c62bf6b9f2bec2709283d3e29b5d
Ilya Dragonromeo <you@example.com>
2024-03-20 16:59:00 +0300

    feat: improved the filter UI UX

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -4,12 +4,16 @@ import style from './Chat.module.scss';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../store';
 import { useState } from 'react';
+import filter_icon from '../../assets/icons/icon_filter.png';
+import back_icon from '../../assets/icons/back_icon.png';
 import date_filter_icon from '../../assets/icons/date_icon.png';
 import user_filter_icon from '../../assets/icons/user_filter_icon.png';
 
 export const Chat = () => {
+  // Component is so big. Need to moved logic with filters in new one
   const [isShowFilter, setIsShowFilter] = useState(false);
-  const [isShowUserFilter, setIsShowUserFilter] = useState(false);
+  const [isShowDate, setIsShowDate] = useState(false);
+  const [isShowUser, setIsShowUser] = useState(false);
   /* I should replace this 2 state (start & end) for 1 when I start refactoring.*/
   const [startDate, setStartData] = useState<string | null>(null);
   const [endDate, setEndData] = useState<string | null>(null);
@@ -22,78 +26,93 @@ export const Chat = () => {
     setIsShowFilter((prevState) => !prevState);
   };
 
+  const handleSwitchDate = () => {
+    setIsShowDate((prevState) => !prevState);
+  };
+
   const handleSwitchUser = () => {
-    setIsShowUserFilter((prevState) => !prevState);
+    setIsShowUser((prevState) => !prevState);
   };
 
   return (
     <div className={style.chat}>
       <div className={style.chat_info}>
         <span>{user?.displayName}</span>
-        {user?.displayName && (
-          <div className={style.filters_container}>
-            {isShowFilter ? (
-              <div className={style.chat_icons}>
-                <div
-                  className={style.close_filter}
-                  onClick={handleSwitchFilter}
-                ></div>
-                <div className={style.date_container}>
-                  <span>Start</span>
-                  <input
-                    type='date'
-                    onChange={(e) => setStartData(e.target.value)}
-                  />
-                </div>
-                <div className={style.date_container}>
-                  <span>End</span>
-                  <input
-                    type='date'
-                    onChange={(e) => setEndData(e.target.value)}
-                  />
-                </div>
+        {user?.displayName &&
+          (isShowFilter ? (
+            <button className={style.filter} onClick={handleSwitchFilter}>
+              <img src={filter_icon} alt='filter' />
+            </button>
+          ) : (
+            <div className={style.filters_container}>
+              <div
+                className={`${style.filter} ${style.filter2}`}
+                onClick={handleSwitchFilter}
+              >
+                <img src={back_icon} alt='back' />
               </div>
-            ) : (
-              <button className={style.filter} onClick={handleSwitchFilter}>
-                <img src={date_filter_icon} alt='date_filter' />
-              </button>
-            )}
+              {isShowDate ? (
+                <div className={style.chat_icons}>
+                  <div
+                    className={style.close_filter}
+                    onClick={handleSwitchDate}
+                  ></div>
+                  <div className={style.date_container}>
+                    <span>Start</span>
+                    <input
+                      type='date'
+                      onChange={(e) => setStartData(e.target.value)}
+                    />
+                  </div>
+                  <div className={style.date_container}>
+                    <span>End</span>
+                    <input
+                      type='date'
+                      onChange={(e) => setEndData(e.target.value)}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <button className={style.filter} onClick={handleSwitchDate}>
+                  <img src={date_filter_icon} alt='date_filter' />
+                </button>
+              )}
 
-            {isShowUserFilter ? (
-              <div className={style.user_filter_container}>
-                <div
-                  className={style.user_filter}
-                  onClick={() => {
-                    user.displayName && setUserFilterName(user.displayName);
-                    handleSwitchUser();
-                  }}
-                >
-                  <img src={user?.photoURL} alt='user_icon' />
-                  <span>{user?.displayName}</span>
+              {isShowUser ? (
+                <div className={style.user_filter_container}>
+                  <div
+                    className={style.user_filter}
+                    onClick={() => {
+                      user.displayName && setUserFilterName(user.displayName);
+                      handleSwitchUser();
+                    }}
+                  >
+                    <img src={user?.photoURL} alt='user_icon' />
+                    <span>{user?.displayName}</span>
+                  </div>
+                  <div
+                    className={style.user_filter}
+                    onClick={() => {
+                      chatOwner.displayName &&
+                        setUserFilterName(chatOwner.displayName);
+                      handleSwitchUser();
+                    }}
+                  >
+                    <img src={chatOwner?.photoURL} alt='chatOwner_icon' />
+                    <span>{chatOwner?.displayName}</span>
+                  </div>
+                  <div
+                    className={`${style.close_filter} ${style.close_filter_2}`}
+                    onClick={handleSwitchUser}
+                  ></div>
                 </div>
-                <div
-                  className={style.user_filter}
-                  onClick={() => {
-                    chatOwner.displayName &&
-                      setUserFilterName(chatOwner.displayName);
-                    handleSwitchUser();
-                  }}
-                >
-                  <img src={chatOwner?.photoURL} alt='chatOwner_icon' />
-                  <span>{chatOwner?.displayName}</span>
-                </div>
-                <div
-                  className={`${style.close_filter} ${style.close_filter_2}`}
-                  onClick={handleSwitchUser}
-                ></div>
-              </div>
-            ) : (
-              <button className={style.filter} onClick={handleSwitchUser}>
-                <img src={user_filter_icon} alt='user_filter' />
-              </button>
-            )}
-          </div>
-        )}
+              ) : (
+                <button className={style.filter} onClick={handleSwitchUser}>
+                  <img src={user_filter_icon} alt='user_filter' />
+                </button>
+              )}
+            </div>
+          ))}
       </div>
 
       <Messages
